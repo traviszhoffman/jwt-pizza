660a3b87d61e99848be5c732fa9f3ff474ef36a9
Travis Hoffman <traviszhoffman@gmail.com>
2026-02-03 16:47:45 -0700

    edit workflows and coverage to 10 instead so that we can see how much coverage in progress

--- tests/auth.spec.ts
+++ tests/auth.spec.ts
@@ -37,3 +37,25 @@ test('register new user', async ({ page }) => {
   await page.getByRole('link', { name: 'b', exact: true }).click();
   await expect(page.getByText(email)).toBeVisible();
 });
+
+test('login', async ({ page }) => {
+  await page.goto('http://localhost:5173/');
+
+  // const randomSuffix = Math.random().toString(36).substring(2, 7);
+  // const email = `bob${randomSuffix}@gmail.com`;
+
+  // await expect(page.getByRole('link', { name: 'Register' })).toBeVisible();
+  // await page.getByRole('link', { name: 'Register' }).click();
+  // await page.getByRole('textbox', { name: 'Full name' }).click();
+  // await page.getByRole('textbox', { name: 'Full name' }).fill('bob');
+  // await page.getByRole('textbox', { name: 'Email address' }).click();
+  // await page.getByRole('textbox', { name: 'Email address' }).fill(email);
+  // await page.getByRole('textbox', { name: 'Password' }).click();
+  // await page.getByRole('textbox', { name: 'Password' }).fill('monkeypie');
+  // await page.getByRole('button', { name: 'Register' }).click();
+  // await expect(page.getByRole('link', { name: 'b', exact: true })).toBeVisible();
+  // await page.getByRole('link', { name: 'b', exact: true }).click();
+  // await expect(page.getByText(email)).toBeVisible();
+
+  
+});
